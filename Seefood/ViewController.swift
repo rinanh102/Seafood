11d50faf0747803bad4069927d92c2d9bc2df2eb
Henry Nguyen <nvqdung@gmail.com>
2018-12-28 11:25:21 -0500

    Initial Commit

---
+++ Seefood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Seefood
+//
+//  Created by henry on 28/12/2018.
+//  Copyright © 2018 HenryNguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
