07d6a9298dc522b5b033d2d5c4b7431ee8a9f826
Henry Nguyen <nvqdung@gmail.com>
2018-12-30 17:28:33 -0500

    Completed

--- Seefood/ViewController.swift
+++ Seefood/ViewController.swift
@@ -54,6 +54,16 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                 fatalError("Model failed to process image.")
             }
             print(results)
+//            if let firstResult = results.first {
+//                if firstResult.identifier.contains("hotdog"){
+//                    self.navigationItem.title = "Hotdog!"
+//                }else{
+//                    self.navigationItem.title = "Not Hotdog!"
+//                }
+//            }
+            if let firstResult = results.first{
+                self.navigationItem.title = firstResult.identifier
+            }
         }
         // We're going to create a handler that specifies the image we want to classify.
         let hander = VNImageRequestHandler(ciImage: image)
